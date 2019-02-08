59321edd3c226f1cfb5107f378e0e5fed5efa035
shawnathanmcfly <sachimasi9187@gmail.com>
2019-02-07 20:11:44 -0500

    fixed dir

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(__dirname + '/public'));
+
 require('./routing/htmlroutes')(app);
 require('./routing/apiroutes')(app);
 
